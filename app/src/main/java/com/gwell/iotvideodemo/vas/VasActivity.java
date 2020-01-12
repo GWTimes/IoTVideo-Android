d6a39f9d93c9135158360b7d9cea31776a94a6a7
GibbsQin <l>
2020-01-12 11:39:50 +0800

    解决Android10.0兼容性问题

--- app/src/main/java/com/gwell/iotvideodemo/vas/VasActivity.java
+++ app/src/main/java/com/gwell/iotvideodemo/vas/VasActivity.java
@@ -147,7 +147,7 @@ public class VasActivity extends BaseActivity implements View.OnClickListener {
             //获取剪贴板管理器：
             ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
             // 创建普通字符型ClipData
-            ClipData mClipData = ClipData.newPlainText("Label", "这里是要复制的文字");
+            ClipData mClipData = ClipData.newPlainText("Label", mTvPlaybackList.getText().toString());
             // 将ClipData内容放到系统剪贴板里。
             cm.setPrimaryClip(mClipData);
         }
