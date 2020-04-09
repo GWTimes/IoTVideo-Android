3e3469b182ea49e2a3c5d29aea8eded530cc02a6
GibbsQin <l>
2020-04-09 11:36:09 +0800

    更新解码配置

--- app/src/main/java/com/tencentcs/iotvideodemo/utils/AppConfig.java
+++ app/src/main/java/com/tencentcs/iotvideodemo/utils/AppConfig.java
@@ -1,7 +1,7 @@
 package com.tencentcs.iotvideodemo.utils;
 
 public class AppConfig {
-    public static final boolean USE_MEIDACODEC = true;
+    public static final boolean USE_MEIDACODEC = false;
     public static final boolean RENDER_DIRECTLY = true;//设置USE_MEIDACODEC为true时才有效
-    public static final int RENDER_DIRECTLY_TYPE = 1;//设置RENDER_DIRECTLY为true时才有效, 0 TexureView, 1 GLSurfaceView
+    public static final int RENDER_DIRECTLY_TYPE = 0;//设置RENDER_DIRECTLY为true时才有效, 0 TexureView, 1 GLSurfaceView
 }
